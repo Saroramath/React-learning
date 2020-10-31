640b50669dff3441236a8e3d20698c57f5ff05cd
Shivam <shivam.iiserm@gmail.com>
2020-10-31 14:45:40 -0230

    this

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       
       <Nav/>
-    <Counter/>
+    
          
           <BrowserRouter>
             <Route exact path = "/" component = {Counter} />
