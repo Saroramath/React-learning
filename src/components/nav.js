4413f43c047d020bf86644b509046f65e7f4bbd1
Shivam <shivam.iiserm@gmail.com>
2020-10-31 15:39:03 -0230

    n

--- src/components/nav.js
+++ src/components/nav.js
@@ -7,9 +7,9 @@ class Nav extends Component {
         return <div>
         <nav>  
          <ul>
-         <li class="crumb"><a href = "/">Home</a> </li>
-         <li class="crumb"><a href = "/ask">Ask the community</a></li>
-        <li class="crumb"><a href = "/postissue">Post an issue</a></li>   
+         <li class="crumb"><a href = "https://saroramath.github.io/React-learning/">Home</a> </li>
+         <li class="crumb"><a href = "https://saroramath.github.io/React-learning/ask">Ask the community</a></li>
+        <li class="crumb"><a href = "https://saroramath.github.io/React-learning/postissue">Post an issue</a></li>   
          </ul>   
            
           </nav> </div>;
