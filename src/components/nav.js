c39478335109f17dbc2ba5b849c0ff9b9ac82058
Shivam <shivam.iiserm@gmail.com>
2020-10-31 22:49:03 -0230

    m

--- src/components/nav.js
+++ src/components/nav.js
@@ -7,9 +7,9 @@ class Nav extends Component {
         return <div>
         <nav>  
          <ul>
-         <li class="crumb"><a href = "React-learning/">Home</a> </li>
-         <li class="crumb"><a href = "React-learning/ask">Ask the community</a></li>
-        <li class="crumb"><a href = "React-learning/postissue">Post an issue</a></li>   
+         <li class="crumb"><a href = "/">Home</a> </li>
+         <li class="crumb"><a href = "/ask">Ask the community</a></li>
+        <li class="crumb"><a href = "/postissue">Post an issue</a></li>   
          </ul>   
            
           </nav> </div>;
